5c94a5ec136860a48902dd7cf6e76fae96d73c95
8922033 <8922033@ed.tus.ac.jp>
2026-06-19 23:38:05 +0900

    Switch database to PostgreSQL

--- app.py
+++ app.py
@@ -31,10 +31,7 @@ client = genai.Client(
 )
 
 app = Flask(__name__)
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DB_PATH = os.path.join(BASE_DIR, "database", "app.db")
-
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
